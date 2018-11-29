f1a8969a9d3b0ad372ebc6392c0295458de85829
invertednoob <invertednoob@users.noreply.github.com>
2018-11-29 10:16:39 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
-cd openIDP/service/Services/entities
-mvn clean install -Dmaven.test.skip=True
-cd target
+cd openIDP/service/Services
+mvn clean install
+cd /entities/target
 cp entities-3.0.jar ../../../../dsl/lib/
-cd openIDP/dsl
-pwd
-gradle build
+cd ../../../../dsl
+pwd gradle build
+cd ../service/Cloud-Config
+mvn install -Dmaven.test.skip=true 
+cd ../eureka-service 
+mvn install -Dmaven.test.skip=true 
+cd ../../dashboard 
+mvn install -Dmaven.test.skip=true 
+cd ../ui 
+mvn install -Dmaven.test.skip=true 
+cd ../scheduler/SchedulerService 
+mvn install -Dmaven.test.skip=true 
+cd ../../jenkins/custom_tools/DevopsJsonConv 
+mvn install -Dmaven.test.skip=true 
+cd ../MetricsProcessor 
+mvn install -Dmaven.test.skip=true 
+cd ../ReportFetchUtil 
+mvn install -Dmaven.test.skip=true 
+cd ../SchedulerUtility 
+mvn install -Dmaven.test.skip=true 
+cd ../../../subscription 
+mvn install -Dmaven.test.skip=true
